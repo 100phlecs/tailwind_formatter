ccba89bc98bf1123bb5a11983c4093cd38de1d4d
100phlecs <hello@based.town>
2022-07-23 10:39:01 -0400

    Initial commit

---
+++ lib/tailwind_formatter.ex
@@ -0,0 +1,18 @@
+defmodule TailwindFormatter do
+  @moduledoc """
+  Documentation for `TailwindFormatter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TailwindFormatter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tailwind_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule TailwindFormatterTest do
+  use ExUnit.Case
+  doctest TailwindFormatter
+
+  test "greets the world" do
+    assert TailwindFormatter.hello() == :world
+  end
+end
