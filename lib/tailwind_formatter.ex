b09228afd5cd7387a1af3546abb85464a143d64e
100phlecs <hello@based.town>
2022-07-24 11:17:19 -0400

    pipe in format to standalone

--- lib/tailwind_formatter.ex
+++ lib/tailwind_formatter.ex
@@ -3,16 +3,74 @@ defmodule TailwindFormatter do
   Documentation for `TailwindFormatter`.
   """
 
+  @behaviour Mix.Tasks.Format
+
+  @impl Mix.Tasks.Format
+  def features(_opts) do
+    [sigils: [:H], extensions: [".heex"]]
+  end
+
+  @impl Mix.Tasks.Format
+  def format(contents, opts) do
+    run(:default, [contents])
+  end
+
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TailwindFormatter.hello()
-      :world
-
+  Returns the path to the executable.
+  The executable may not be available if it was not yet installed.
   """
-  def hello do
-    :world
+  def bin_path do
+    name = "tailwindsort-#{target()}"
+
+    Application.get_env(:tailwindsort, :path) ||
+      if Code.ensure_loaded?(Mix.Project) do
+        Path.join(Path.dirname(Mix.Project.build_path()), name)
+      else
+        Path.expand("_build/#{name}")
+      end
+  end
+
+  @doc """
+  Runs the given command with `args`.
+  The given args will be appended to the configured args.
+  """
+  def run(profile, extra_args) when is_atom(profile) and is_list(extra_args) do
+    config = [] # config_for!(profile)
+    args = config[:args] || ["--config", "assets/tailwind.config.js"]
+
+    opts = [
+      cd: config[:cd] || File.cwd!(),
+      env: config[:env] || %{},
+      stderr_to_stdout: true
+    ]
+    
+    IO.inspect(bin_path())
+    IO.inspect(File.cwd!())
+    result = bin_path()
+    |> System.cmd(args ++ extra_args, opts)
+    IO.inspect(result)
+    elem(result, 0)
+  end
+
+  # Available targets:
+  #  tailwindsort-linux-arm64
+  #  tailwindsort-linux-x64
+  #  tailwindsort-macos-arm64
+  #  tailwindsort-macos-x64
+  #  tailwindsort-windows-x64.exe
+  defp target do
+    arch_str = :erlang.system_info(:system_architecture)
+    [arch | _] = arch_str |> List.to_string() |> String.split("-")
+
+    case {:os.type(), arch, :erlang.system_info(:wordsize) * 8} do
+      {{:win32, _}, _arch, 64} -> "windows-x64.exe"
+      {{:unix, :darwin}, arch, 64} when arch in ~w(arm aarch64) -> "macos-arm64"
+      {{:unix, :darwin}, "x86_64", 64} -> "macos-x64"
+      {{:unix, :linux}, "aarch64", 64} -> "linux-arm64"
+      {{:unix, _osname}, arch, 64} when arch in ~w(x86_64 amd64) -> "linux-x64"
+      {_os, _arch, _wordsize} -> raise "tailwind is not available for architecture: #{arch_str}"
+    end
   end
 end
+
--- test/tailwind_formatter_test.exs
+++ test/tailwind_formatter_test.exs
@@ -2,7 +2,35 @@ defmodule TailwindFormatterTest do
   use ExUnit.Case
   doctest TailwindFormatter
 
-  test "greets the world" do
-    assert TailwindFormatter.hello() == :world
+
+  alias TailwindFormatter
+
+  defp assert_formatter_output(input, expected, dot_formatter_opts \\ []) do
+    first_pass = TailwindFormatter.format(input, dot_formatter_opts)
+    assert first_pass == expected
+
+    second_pass = TailwindFormatter.format(first_pass, dot_formatter_opts)
+    assert second_pass == expected
+  end
+
+  def assert_formatter_doesnt_change(code, dot_formatter_opts \\ []) do
+    first_pass = TailwindFormatter.format(code, dot_formatter_opts)
+    assert first_pass == code
+
+    second_pass = TailwindFormatter.format(first_pass, dot_formatter_opts)
+    assert second_pass == code
+  end
+
+
+  test "works without config" do
+    input = """
+    <div class="sm:lowercase uppercase potato text-sm"></div>
+    """
+
+    expected = """
+    <div class="potato text-sm uppercase sm:lowercase"></div>
+    """
+
+    assert_formatter_output(input, expected)
   end
 end
