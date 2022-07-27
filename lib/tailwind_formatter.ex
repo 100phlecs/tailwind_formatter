a877d1db67d440e234d082b98c017e512b78c281
100phlecs <hello@based.town>
2022-07-27 18:13:26 -0400

    clean up variant sort func

--- lib/tailwind_formatter.ex
+++ lib/tailwind_formatter.ex
@@ -18,24 +18,7 @@ defmodule TailwindFormatter do
 
       base_sorted = sort_base_classes(base_classes) |> Enum.join(" ")
 
-      variant_sorted =
-        Enum.map(variants, fn variant_class ->
-          String.split(variant_class, ":")
-        end)
-        |> Enum.group_by(&List.first/1, &List.last/1)
-        |> Map.to_list()
-        |> sort_variant_groups
-        |> Enum.map(fn variant_group ->
-          {variant, base_classes} = variant_group
-
-          sorted_classes = sort_base_classes(base_classes)
-
-          Enum.map(sorted_classes, fn class ->
-            "#{variant}:#{class}"
-          end)
-          |> Enum.join(" ")
-        end)
-        |> Enum.join(" ")
+      variant_sorted = sort_variant_classes(variants) |> Enum.join(" ")
 
       sorted_classes =
         (base_sorted <> " " <> variant_sorted)
@@ -45,17 +28,35 @@ defmodule TailwindFormatter do
     end)
   end
 
-  defp sort_variant_groups(variant_groups) do
-    Enum.map(variant_groups, fn variant_group ->
-      {Map.get(Defaults.variant_order(), elem(variant_group, 0), -1), variant_group}
+  defp sort_variant_classes(variants) do
+    Enum.map(variants, fn variant_class ->
+      String.split(variant_class, ":")
+    end)
+    |> Enum.group_by(&List.first/1, &List.last/1)
+    |> Map.to_list()
+    |> Enum.map(fn variant_group ->
+      variant = elem(variant_group, 0)
+      sort_number = Map.get(Defaults.variant_order(), variant, -1)
+      {sort_number, variant_group}
     end)
     |> Enum.sort(&(elem(&1, 0) <= elem(&2, 0)))
     |> Enum.map(&elem(&1, 1))
+    |> Enum.map(fn variant_group ->
+      {variant, base_classes} = variant_group
+
+      sorted_classes = sort_base_classes(base_classes)
+
+      Enum.map(sorted_classes, fn class ->
+        "#{variant}:#{class}"
+      end)
+      |> Enum.join(" ")
+    end)
   end
 
   defp sort_base_classes(base_classes) do
     Enum.map(base_classes, fn class ->
-      {Map.get(Defaults.class_order(), class, -1), class}
+      sort_number = Map.get(Defaults.class_order(), class, -1)
+      {sort_number, class}
     end)
     |> Enum.sort(&(elem(&1, 0) <= elem(&2, 0)))
     |> Enum.map(&elem(&1, 1))
