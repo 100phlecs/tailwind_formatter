c8319f48b9173e0935633d04397acca29eb1705c
100phlecs <hello@based.town>
2022-07-27 20:04:05 -0400

    handles chained variants, switch to recursive sort

--- lib/tailwind_formatter.ex
+++ lib/tailwind_formatter.ex
@@ -14,43 +14,22 @@ defmodule TailwindFormatter do
 
   def format(contents, _opts) do
     Regex.replace(Defaults.regex_pattern(), contents, fn original, _split, classes ->
-      {base_classes, variants} = separate(classes)
+      class_list = String.split(classes)
+      sorted_list = sort(class_list) |> Enum.join(" ")
 
-      base_sorted = sort_base_classes(base_classes) |> Enum.join(" ")
-
-      variant_sorted = sort_variant_classes(variants) |> Enum.join(" ")
-
-      sorted_classes =
-        (base_sorted <> " " <> variant_sorted)
-        |> String.trim()
-
-      String.replace(original, ~r/"([^"]*)"/, "\"" <> sorted_classes <> "\"")
+      String.replace(original, ~r/"([^"]*)"/, "\"" <> sorted_list <> "\"")
     end)
   end
 
-  defp sort_variant_classes(variants) do
-    Enum.map(variants, fn variant_class ->
-      String.split(variant_class, ":")
-    end)
-    |> Enum.group_by(&List.first/1, &List.last/1)
-    |> Map.to_list()
-    |> Enum.map(fn variant_group ->
-      variant = elem(variant_group, 0)
-      sort_number = Map.get(Defaults.variant_order(), variant, -1)
-      {sort_number, variant_group}
-    end)
-    |> Enum.sort(&(elem(&1, 0) <= elem(&2, 0)))
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.map(fn variant_group ->
-      {variant, base_classes} = variant_group
+  defp sort([]) do
+  end
 
-      sorted_classes = sort_base_classes(base_classes)
+  defp sort(class_list) do
+    {base_classes, variants} = separate(class_list)
+    base_sorted = sort_base_classes(base_classes)
+    variant_sorted = sort_variant_classes(variants)
 
-      Enum.map(sorted_classes, fn class ->
-        "#{variant}:#{class}"
-      end)
-      |> Enum.join(" ")
-    end)
+    base_sorted ++ variant_sorted
   end
 
   defp sort_base_classes(base_classes) do
@@ -63,9 +42,8 @@ defmodule TailwindFormatter do
     |> List.flatten()
   end
 
-  defp separate(classes) do
-    classes
-    |> String.split(" ")
+  defp separate(class_list) do
+    class_list
     |> Enum.reduce({[], []}, fn class, tuple ->
       if variant?(class) do
         put_elem(tuple, 1, [class | elem(tuple, 1)])
@@ -77,5 +55,49 @@ defmodule TailwindFormatter do
 
   defp variant?(class) do
     String.contains?(class, ":")
+  end
+
+  defp sort_variant_classes(variants) do
+    variants
+    |> group_by_variant()
+    |> sort_by_variant_order()
+    |> sort_classes_per_variant()
+    |> grouped_variants_to_list()
+  end
+
+  defp group_by_variant(variants) do
+    variants
+    |> Enum.map(&String.split(&1, ":", parts: 2))
+    |> Enum.group_by(&List.first/1, &List.last/1)
+  end
+
+  defp sort_by_variant_order(variant_groups) do
+    variant_groups
+    |> Enum.map(fn variant_group ->
+      variant = elem(variant_group, 0)
+      sort_number = Map.get(Defaults.variant_order(), variant, -1)
+
+      {sort_number, variant_group}
+    end)
+    |> Enum.sort(&(elem(&1, 0) <= elem(&2, 0)))
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  defp sort_classes_per_variant(grouped_variants) do
+    Enum.map(grouped_variants, fn variant_group ->
+      {variant, base_classes} = variant_group
+      {variant, sort(base_classes)}
+    end)
+  end
+
+  defp grouped_variants_to_list(grouped_variants) do
+    Enum.map(grouped_variants, fn variant_group ->
+      {variant, base_classes} = variant_group
+
+      Enum.map(base_classes, fn class ->
+        "#{variant}:#{class}"
+      end)
+    end)
+    |> List.flatten()
   end
 end
--- test/tailwind_formatter_test.exs
+++ test/tailwind_formatter_test.exs
@@ -20,13 +20,39 @@ defmodule TailwindFormatterTest do
     assert second_pass == code
   end
 
-  test "works without config" do
+  test "works" do
     input = """
     <div class="text-sm potato sm:lowercase uppercase"></div>
     """
 
     expected = """
     <div class="potato text-sm uppercase sm:lowercase"></div>
+    """
+
+    assert_formatter_output(input, expected)
+  end
+
+  test "handles chained variants" do
+    input = """
+    <div class="text-sm odd:decoration-slate-50 sm:hover:bg-gray-500 tomato sm:lowercase uppercase sm:hover:bg-unknown-500 disabled:sm:text-lg dark:disabled:sm:lg:group-hover:text-blue-500"></div>
+    """
+
+    expected = """
+    <div class="tomato text-sm uppercase sm:lowercase sm:hover:bg-unknown-500 sm:hover:bg-gray-500 dark:disabled:sm:lg:group-hover:text-blue-500 odd:decoration-slate-50 disabled:sm:text-lg"></div>
+    """
+
+    assert_formatter_output(input, expected)
+  end
+
+  test "removes excess whitespace" do
+    input = """
+    <div class="text-sm     sm:hover:bg-gray-500 tomato     sm:lowercase uppercase sm:hover:bg-unknown-500    disabled:sm:text-lg
+
+    "></div>
+    """
+
+    expected = """
+    <div class="tomato text-sm uppercase sm:lowercase sm:hover:bg-unknown-500 sm:hover:bg-gray-500 disabled:sm:text-lg"></div>
     """
 
     assert_formatter_output(input, expected)
