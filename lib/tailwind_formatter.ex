4031b58182cf3f811978b7c2f1fe5cfb51f86a27
100phlecs <24684823+100phlecs@users.noreply.github.com>
2023-10-04 23:14:52 -0400

    handle nested blocks

--- lib/tailwind_formatter.ex
+++ lib/tailwind_formatter.ex
@@ -48,7 +48,8 @@ defmodule TailwindFormatter do
 
   defp sort_expr({:<>, meta, children}), do: {:<>, meta, handle_concatenation(children)}
   defp sort_expr({:<<>>, meta, children}), do: {:<<>>, meta, handle_interpolation(children)}
-  defp sort_expr({:__block__, meta, [text]}), do: {:__block__, meta, [sort(text)]}
+  defp sort_expr({:__block__, meta, [text]}), do: {:__block__, meta, [sort_expr(text)]}
+  defp sort_expr(text) when is_binary(text), do: sort(text)
   defp sort_expr(node), do: node
 
   defp handle_concatenation(children) do
