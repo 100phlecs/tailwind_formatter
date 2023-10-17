bf9a29fe3be77420bb6a7d7cd3b512fd42b777ef
100phlecs <24684823+100phlecs@users.noreply.github.com>
2023-10-17 17:45:12 -0400

    format many files

--- bench/format.exs
+++ bench/format.exs
@@ -2,9 +2,16 @@ input =
   File.read!("test/tailwind_formatter_test.exs")
   |> String.split("\"\"\"")
   |> Enum.drop_every(2)
+
+big_file_input  =
+  input
   |> Enum.join()
-  |> String.duplicate(1000)
+  |> String.duplicate(250)
+
+many_file_input =
+  Enum.reduce(1..250, input, fn _i, acc -> input ++ acc end)
 
 Benchee.run(%{
-  "format" => fn -> TailwindFormatter.format(input, []) end
+  "format big file" => fn -> TailwindFormatter.format(big_file_input, []) end,
+  "format many files" => fn -> Enum.each(many_file_input, &TailwindFormatter.format(&1, [])) end
 })
