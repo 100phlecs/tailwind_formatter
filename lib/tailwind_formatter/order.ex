1a2c3b34e20a84d8add3c0d722816cf61ee3c301
100phlecs <24684823+100phlecs@users.noreply.github.com>
2023-10-22 15:37:36 -0400

    source to local project

--- lib/tailwind_formatter/order.ex
+++ lib/tailwind_formatter/order.ex
@@ -1,9 +1,14 @@
 defmodule TailwindFormatter.Order do
-  @external_resource "_build/classes.txt"
-  @external_resource "_build/variants.txt"
+  custom_classes = Path.join(Path.dirname(Mix.Project.build_path()), "classes.txt")
+  custom_variants = Path.join(Path.dirname(Mix.Project.build_path()), "variants.txt")
+
+  @external_resource custom_classes
+  @external_resource custom_variants
 
   @moduledoc false
   order_map = fn src ->
+    Mix.shell().info([:green, "Loading in #{src}."])
+
     src
     |> File.read!()
     |> String.split(~r/\R/)
@@ -11,12 +16,12 @@ defmodule TailwindFormatter.Order do
     |> Map.new()
   end
 
-  @classes if File.exists?("_build/classes.txt"),
-             do: order_map.("_build/classes.txt"),
-             else: order_map.("priv/variants.txt")
+  @classes if File.exists?(custom_classes),
+             do: order_map.(custom_classes),
+             else: order_map.("priv/classes.txt")
 
-  @variants if File.exists?("_build/variants.txt"),
-              do: order_map.("_build/variants.txt"),
+  @variants if File.exists?(custom_variants),
+              do: order_map.(custom_variants),
               else: order_map.("priv/variants.txt")
 
   def variants(), do: @variants
